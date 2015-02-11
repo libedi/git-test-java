046a30032bf3f2afee7e93af909add716c6c787d
libedi <libedi@gmail.com>
2015-02-11 15:19:11 +0900

    modify 1

--- git-test-java/src/main/TestClass.java
+++ git-test-java/src/main/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Git Test.");
+		System.out.println("Modify 1");
 	}
 
 }
